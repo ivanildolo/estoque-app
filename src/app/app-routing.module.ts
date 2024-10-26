f6324e3c48340b2a8fc2796f67f3e0640dedbacf
ivanildolo <ivanildosilvalima@gmail.com>
2024-10-26 10:37:18 -0300

    feat(estrutura): Adiciona projeto ao git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'estoque', loadChildren: () => import('./estoque/estoque.module').then(m => m.EstoqueModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
